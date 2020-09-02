7ce128107510528a24adf1e367ca8a0b0dbc6669
student <protogeyorocks@gmail.com>
2020-09-02 15:36:01 -0600

    fixed?

--- wordCount.py
+++ wordCount.py
@@ -5,25 +5,22 @@ import string
 textFname = sys.argv[1]
 outputFname = sys.argv[2]
 
-#file = open(textFname, "r")
 d = dict()
-with open(textFname, 'r') as file:
+with open(textFname, 'r')as file:
     for line in file:
         line = line.strip()
         line = line.lower()
         line = line.translate(line.maketrans("", "",string.punctuation))
         words = line.split(" ")
         for word in words:
-            print(word)
-            if word in d:
-                d[word] = d[word]+1
-            else:
-                d[word] = 1
-
+            if len(word) >= 1:
+                if word in d:
+                    d[word] = d[word]+1
+                else:
+                    d[word] = 1    
 eraseFile = open(outputFname, 'w').close()
 f = open(outputFname, "a")
 for key in sorted(d.keys()):
-    key = key.strip()
     f.write(key+" "+str(d[key])+"\n")
+
 f.close()
-file.close()

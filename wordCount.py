4089ffbb221dc0b0b01caaf206bf120d4a286c6e
Protogey <47206443+Protogey@users.noreply.github.com>
2020-09-14 15:03:27 -0600

    Added additional comments

--- wordCount.py
+++ wordCount.py
@@ -4,7 +4,7 @@ import string
 
 textFname = sys.argv[1]
 outputFname = sys.argv[2]
-
+#dict data structure to hold words and number of them in text file.
 d = dict()
 with open(textFname, 'r')as file:
     for line in file:
@@ -14,6 +14,8 @@ with open(textFname, 'r')as file:
         words = line.split(" ")
         for word in words:
             if len(word) >= 1:
+                #here if word is found in d, it adds 1 to its count
+                #else it adds the word and sets it to 1.
                 if word in d:
                     d[word] = d[word]+1
                 else:
